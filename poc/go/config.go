84359a2ba9a922ff457a2ebc4ab965cff601b7e0
stephan.kempkes@gmail.com <stephan.kempkes@gmail.com>
2021-07-05 23:58:13 +0200

    refactor: replace tabs

--- poc/go/config.go
+++ poc/go/config.go
@@ -1,10 +1,10 @@
 package main
 
 type Config struct {
-	Bootstrap string
-	LogLevel  string
-	Mode      string
-	Port      int
-	RpcPort   int
-	Seed      int64
+    Bootstrap string
+    LogLevel  string
+    Mode      string
+    Port      int
+    RpcPort   int
+    Seed      int64
 }
